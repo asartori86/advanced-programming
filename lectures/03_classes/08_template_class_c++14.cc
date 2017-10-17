f53b0b0b4f3b4aa01db505e1e166d0371206d75c
Alberto Sartori <alberto.sartori86@gmail.com>
2017-10-17 12:31:08 +0200

    c++14

---
+++ lectures/03_classes/08_template_class_c++14.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+template <typename num>
+class Vector {
+  num* elem;
+  unsigned int _size;
+
+ public:
+  Vector(const unsigned int size) : _size{size}, elem{new num[size]} {}
+
+  ~Vector() { delete[] elem; }
+
+  auto size() const { return _size; }
+
+  auto& operator[](const unsigned int i) { return elem[i]; }
+
+  const auto& operator[](const unsigned int i) const { return elem[i]; }
+
+  void pretty_print() const;
+};
+
+int main() {
+  Vector<double> v{10};
+
+  for (auto i = 0; i < v.size(); ++i)
+    v[i] = i;
+
+  v.pretty_print();
+
+  Vector<double>* pv = &v;
+
+  pv->pretty_print();
+
+  return 0;
+}
+
+template <typename num>
+void Vector<num>::pretty_print() const {
+  std::cout << "Vector allocated at " << elem << std::endl;
+  for (auto i = 0; i < _size; ++i)
+    std::cout << "elem[" << i << "] = " << elem[i] << std::endl;
+}
