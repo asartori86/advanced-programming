d57cee9212c2b2326f4f08fa11065449bee7381b
Alberto Sartori <alberto.sartori86@gmail.com>
2017-10-22 09:42:36 +0200

    stack unwinding

--- lectures/04_error_handling/06_stack_unwinding.cc
+++ lectures/04_error_handling/06_stack_unwinding.cc
@@ -1,6 +1,19 @@
-#include "ap_error.h"
 #include <iostream>
 #include <vector>
+
+#include "ap_error.h"
+
+class Foo {
+ public:
+  Foo() { std::cout << "Foo\n"; }
+  ~Foo() { std::cout << "~Foo\n"; }
+};
+
+class Bar {
+ public:
+  Bar() { std::cout << "Bar\n"; }
+  ~Bar() { std::cout << "~Bar\n"; }
+};
 
 class Vector {
   double* elem;
@@ -18,20 +31,28 @@ class ManyResources {
   ManyResources() : v{3} {
     try {
       second = new double[5];
-      AP_error("AP_error in ManyResources ctor\n");
+      AP_error("Error in ManyResources ctor\n");
     } catch (...) {
       delete[] second;
       throw;
     }
   }
+
   ~ManyResources() noexcept { delete[] second; }
 };
 
 int main() {
+  Foo f;
+  int* raw_ptr = new int[7];
   try {
+    // int * raw_ptr=new int[7]; // wrong because raw_ptr would not be visible
+    // inside the catch-clause
     ManyResources mr;
+    Bar b;
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
+    delete[] raw_ptr;  // house clening
+    return 1;
   }
   return 0;
 }
